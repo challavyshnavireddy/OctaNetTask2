8f7d8b178d546fc2e4b09146368d29746ef1f6f4
challavyshnavireddy <chvry03@gmail.com>
2023-06-04 21:48:58 +0530

    added done feature

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,6 +1,15 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { IoIosRefresh, IoMdDoneAll } from "react-icons/io";
 export const TododList = ({ todolist, deleteHandler }) => {
+  const [icon, setIcon] = useState(
+    new Array(todolist.length).fill(<IoIosRefresh />)
+  );
+
+  const getsetIcon = (index) => {
+    const newIcon = [...icon];
+    newIcon[index] = <IoMdDoneAll />;
+    setIcon(newIcon);
+  };
   {
     if (todolist.length === 0) {
       return <h3 className="t mt-5">NO Tasks Yet...!!</h3>;
@@ -12,12 +21,19 @@ export const TododList = ({ todolist, deleteHandler }) => {
             {todolist.map((todo, index) => (
               <div key={index}>
                 <h5 className="text">
-                  ✯{todo}{" "}
+                  {icon[index]} {todo} &nbsp; &nbsp;&nbsp;
                   <button
                     className="btn btn-danger"
                     onClick={() => deleteHandler(index)}
                   >
                     DELETE
+                  </button>{" "}
+                  &nbsp; &nbsp; &nbsp;{" "}
+                  <button
+                    className="btn btn-primary"
+                    onClick={() => getsetIcon(index)}
+                  >
+                    DONE
                   </button>
                 </h5>{" "}
                 &nbsp;
