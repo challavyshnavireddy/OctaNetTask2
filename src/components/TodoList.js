e8cdb57033815ef0e49a025538e2d42a19c4ceef
challavyshnavireddy <chvry03@gmail.com>
2023-06-05 11:52:05 +0530

    remove done

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,15 +1,14 @@
 import React, { useState } from "react";
-import { IoIosRefresh, IoMdDoneAll } from "react-icons/io";
+// import { IoIosRefresh, IoMdDoneAll } from "react-icons/io";
 export const TododList = ({ todolist, deleteHandler }) => {
-  const [icon, setIcon] = useState(
-    new Array(todolist.length).fill(<IoIosRefresh />)
-  );
+  // const [icon, setIcon] = useState( new Array(todolist.length).fill(<IoIosRefresh />)
+  // );
 
-  const getsetIcon = (index) => {
-    const newIcon = [...icon];
-    newIcon[index] = <IoMdDoneAll />;
-    setIcon(newIcon);
-  };
+  // const getsetIcon = (index) => {
+  //   const newIcon=[...icon]
+  //   newIcon[index]=<IoMdDoneAll/>
+  //   setIcon(newIcon)
+  // };
   {
     if (todolist.length === 0) {
       return <h3 className="t mt-5">NO Tasks Yet...!!</h3>;
@@ -21,20 +20,23 @@ export const TododList = ({ todolist, deleteHandler }) => {
             {todolist.map((todo, index) => (
               <div key={index}>
                 <h5 className="text">
-                  {icon[index]} {todo} &nbsp; &nbsp;&nbsp;
+                  {/* {icon[index]}  */}
+                  {todo} &nbsp; &nbsp;&nbsp;
                   <button
                     className="btn btn-danger"
                     onClick={() => deleteHandler(index)}
                   >
                     DELETE
                   </button>{" "}
+                  
                   &nbsp; &nbsp; &nbsp;{" "}
-                  <button
+                  {/* <button
                     className="btn btn-primary"
                     onClick={() => getsetIcon(index)}
                   >
                     DONE
-                  </button>
+                  </button> */}
+
                 </h5>{" "}
                 &nbsp;
               </div>
